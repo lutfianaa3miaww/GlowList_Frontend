d5d5218f7e8ef62ea0341ad0284d29f33758e83f
lutfianaa3miaww <lutfiii9988776655@gmail.com>
2026-08-07 09:52:24 +0700

    update terbaru
    mengerjakan jobsheet fe 1, beserta tugas membuat menu baru di sidebar 'tentang' perkenalan diri singkat

---
+++ glowlist-frontend/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+// Home.jsx
+export default function Home() {
+    return <h2>Selamat Datang di GlowList</h2>
+}
+
---
+++ glowlist-frontend/src/pages/Tentang.jsx
@@ -0,0 +1,16 @@
+export default function Tentang() {
+    return (
+        <div style={{ padding: "20px " }}>
+            <h2>Tentang Saya ۶ৎ</h2>
+            <p><strong>Nama:</strong>Lutfiana Uswatul Khoiriyah</p>
+            <p><strong>Kelas:</strong> XII RPL 1</p>
+            <h3>Alasan Suka Skincare</h3>
+            <p>
+                Saya sangat menyukai skincare karena bagi saya,
+                merawat kulit di pagi dan malam hari adalah bentuk tanggung jawab untuk menjaganya tetap sehat, 
+                terhidrasi, dan bercahaya, sekaligus menjadi ritual me-time menenangkan yang ampuh melepas stres
+                agar saya lebih siap menghadapi hari serta lebih rileks sebelum tidur.
+            </p>
+        </div>
+    )
+}
